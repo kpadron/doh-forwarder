5995d18d517967956dd0484e5153965019190eaa
Kristian Padron <kpadron.github@gmail.com>
2018-08-23 18:10:54 -0400

    updated readme, added tcp forwarding and argparsing

--- README.md
+++ README.md
@@ -3,14 +3,22 @@ DNS over HTTPS forwarder.
 
 doh-async.py seems to be best for my use case.
 
-Notes:
-- aiohttp library is required for certain variants
-	$ sudo apt install python3-pip -y && sudo pip3 install aiohttp
+### Requirements
+- aiohttp library
+	* sudo apt install python3-pip -y && sudo pip3 install aiohttp
 
-TODO:
-- Add install/uninstall script (install as a service via systemd)
-- Test uvloop performance
-- Test aiodns performance
-- Use exceptions to detect connection errors and attempt to reconnect
-- Add argument parsing
-- Add upstream server metrics and heuristics
+### Suggestions
+- uvloop library
+	* minor performance increase:
+	sudo apt install python3-pip -y && sudo pip3 install uvloop
+- aiodns library
+	* slight performance increase:
+	sudo apt install python3-pip -y && sudo pip3 install aiodns
+
+### TODO
+- [x] Add install/uninstall script (install as a service via systemd)
+- [x] Add argument parsing for common configurables
+- [x] Add TCP resolving in addition to UDP resolving
+- [ ] Add DNS packet parsing to print human-readable packets to log
+- [ ] Add upstream server metrics and heuristics
+- [ ] Use exceptions to detect connection errors and attempt to reconnect
--- doh-async.py
+++ doh-async.py
@@ -1,24 +1,52 @@
 #!/usr/bin/env python3
 import asyncio, aiohttp
 import logging, random
+import argparse, struct
 
+# Attempt to use uvloop if installed for extra performance
+try:
+	import uvloop
+	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+	pass
 
-host = '127.0.0.1'
-port = 5053
+# Handle command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--listen-address', default='127.0.0.1',
+					help='address to listen on for DNS over HTTPS server (default: %(default)s)')
+parser.add_argument('-p', '--listen-port', type=int, default=53,
+					help='port to listen on for DNS over HTTPS server (default: %(default)s)')
+parser.add_argument('-u', '--upstreams', nargs='+', default=['https://1.1.1.1/dns-query', 'https://1.0.0.1/dns-query'],
+					help='upstream servers to forward DNS queryies and requests to (default: %(default)s)')
+parser.add_argument('-t', '--tcp', action='store_true', default=False,
+					help='serve TCP based queries and requests along with UDP (default: %(default)s)')
+args = parser.parse_args()
+
+host = args.listen_address
+port = args.listen_port
+upstreams = args.upstreams
+
 headers = {'accept': 'application/dns-message', 'content-type': 'application/dns-message'}
-upstreams = ('https://1.1.1.1/dns-query', 'https://1.0.0.1/dns-query')
 conns = []
 
 
 def main():
 	# Setup logging
-	logging.basicConfig(level='INFO')
+	logging.basicConfig(level='INFO', format='[%(levelname)s] %(message)s')
 
-	# Setup event loop and UDP server
-	logging.info('Starting UDP server listening on: %s#%d' % (host, port))
+	# Setup event loop
 	loop = asyncio.get_event_loop()
-	listen = loop.create_datagram_endpoint(DohProtocol, local_addr = (host, port))
-	transport, protocol = loop.run_until_complete(listen)
+
+	# Setup UDP server
+	logging.info('Starting UDP server listening on: %s#%d' % (host, port))
+	udp_listen = loop.create_datagram_endpoint(UdpDohProtocol, local_addr = (host, port))
+	udp, protocol = loop.run_until_complete(udp_listen)
+
+	# Setup TCP server
+	if args.tcp:
+		logging.info('Starting TCP server listening on %s#%d' % (host, port))
+		tcp_listen = loop.create_server(TcpDohProtocol, host, port)
+		tcp = loop.run_until_complete(tcp_listen)
 
 	# Connect to upstream servers
 	for upstream in upstreams:
@@ -35,14 +63,17 @@ def main():
 	for conn in conns:
 		loop.run_until_complete(upstream_close(conn))
 
-	# Close UDP server and event loop
-	transport.close()
+	# Close listening servers and event loop
+	udp.close()
+	if args.tcp:
+		tcp.close()
+
 	loop.close()
 
 
-class DohProtocol(asyncio.DatagramProtocol):
+class UdpDohProtocol(asyncio.DatagramProtocol):
 	"""
-	DNS over HTTPS protocol to use with asyncio.
+	DNS over HTTPS UDP protocol to use with asyncio.
 	"""
 
 	def connection_made(self, transport):
@@ -52,8 +83,8 @@ class DohProtocol(asyncio.DatagramProtocol):
 		# Schedule packet forwarding coroutine
 		asyncio.ensure_future(self.forward_packet(data, addr))
 
-	def connection_lost(self, exc):
-		pass
+	def error_received(self, exc):
+		logging.warning('Minor transport error')
 
 	async def forward_packet(self, data, addr):
 		# Select upstream server to forward to
@@ -64,6 +95,40 @@ class DohProtocol(asyncio.DatagramProtocol):
 
 		# Send DNS packet to client
 		self.transport.sendto(data, addr)
+
+
+class TcpDohProtocol(asyncio.Protocol):
+	"""
+	DNS over HTTPS TCP protocol to use with asyncio.
+	"""
+
+	def connection_made(self, transport):
+		self.transport = transport
+
+	def data_received(self, data):
+		# Schedule packet forwarding coroutine
+		asyncio.ensure_future(self.forward_packet(data))
+
+	def eof_received(self):
+		if self.transport.can_write_eof():
+			self.transport.write_eof()
+
+	def connection_lost(self, exc):
+		self.transport.close()
+
+	async def forward_packet(self, data):
+		# Select upstream server to forward to
+		index = random.randrange(len(upstreams))
+
+		# Await upstream forwarding coroutine
+		data = await upstream_forward(upstreams[index], data[2:], conns[index])
+
+		# Send DNS packet to client
+		if self.transport.is_reading():
+			self.transport.write(struct.pack('! H', len(data)) + data)
+		else:
+			logging.warning('Client not reading, aborting transport')
+			self.transport.abort()
 
 
 async def upstream_connect():
@@ -98,11 +163,15 @@ async def upstream_forward(url, data, conn):
 
 	# Await upstream response
 	while True:
-		async with conn.post(url, data=data) as response:
-			if response.status == 200:
-				return await response.read()
+		try:
+			async with conn.post(url, data=data) as response:
+				if response.status == 200:
+					return await response.read()
 
-			logging.warning('%s (%d): IN %s, OUT %s' % (url, response.status, data, await response.read()))
+				logging.warning('%s (%d): IN %s, OUT %s' % (url, response.status, data, await response.read()))
+		except aiohttp.ClientConnectionError:
+			logging.exception('Connection error with upstream server: %s', url)
+			return b''
 
 
 async def upstream_close(conn):
