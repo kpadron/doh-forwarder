78c3baf30442296ef8538235d917754c00bfc0c2
Kristian Padron <kpadron.github@gmail.com>
2018-08-22 21:47:10 -0400

    added basic install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+
+systemctl stop doh-forwarder
+systemctl disable doh-forwarder
+cp doh-async.py /usr/local/bin/doh-forwarder
+cp doh-forwarder.service /lib/systemd/system/
+systemctl daemon-reload
+systemctl start doh-forwarder
+systemctl enable doh-forwarder
