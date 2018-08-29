6d48a85eef3ea769ecf9a8c0c0706747b7b61833
Kristian Padron <kpadron.github@gmail.com>
2018-08-29 14:34:19 -0400

    updated with new concepts

--- concepts/doh-async2.py
+++ concepts/doh-async2.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-import asyncio, aiohttp, aioprocessing
+import asyncio, vibora
 import random, struct
 import argparse, logging
 
+
+# Attempt to use uvloop if installed for extra performance
+try:
+	import uvloop
+	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+	pass
 
 # Handle command line arguments
 parser = argparse.ArgumentParser()
@@ -22,9 +29,7 @@ upstreams = args.upstreams
 
 headers = {'accept': 'application/dns-message', 'content-type': 'application/dns-message'}
 conns = []
-
-request_queue = aioprocessing.AioQueue()
-response_queue = aioprocessing.AioQueue()
+rtt = []
 
 
 def main():
@@ -45,45 +50,12 @@ def main():
 		tcp_listen = loop.create_server(TcpDohProtocol, host, port)
 		tcp = loop.run_until_complete(tcp_listen)
 
-	# # Connect to upstream servers
-	# for upstream in upstreams:
-	# 	logging.info('Connecting to upstream server: %s' % (upstream))
-	# 	conns.append(loop.run_until_complete(upstream_connect()))
-
-	# Serve forever
-	try:
-		for _ in range(3):
-			aioprocessing.AioProcess(target=forwarder, daemon=True).start()
-
-		loop.run_forever()
-	except (KeyboardInterrupt, SystemExit):
-		pass
-
-	# # Close upstream connections
-	# for conn in conns:
-	# 	loop.run_until_complete(upstream_close(conn))
-
-	# Close listening servers and event loop
-	udp.close()
-	if args.tcp:
-		tcp.close()
-
-	loop.close()
-
-
-def forwarder():
-	loop = asyncio.new_event_loop()
-	asyncio.set_event_loop(loop)
-
-	conns = []
-
 	# Connect to upstream servers
 	for upstream in upstreams:
 		logging.info('Connecting to upstream server: %s' % (upstream))
-		conns.append(loop.run_until_complete(upstream_connect()))
+		conns.append(loop.run_until_complete(upstream_connect(upstream)))
 
-	asyncio.ensure_future(forward_loop(conns))
-
+	# Serve forever
 	try:
 		loop.run_forever()
 	except (KeyboardInterrupt, SystemExit):
@@ -93,24 +65,12 @@ def forwarder():
 	for conn in conns:
 		loop.run_until_complete(upstream_close(conn))
 
+	# Close listening servers and event loop
+	udp.close()
+	if args.tcp:
+		tcp.close()
 
-async def forward_loop(conns):
-	while True:
-		# Receive requests from the listener
-		data, addr = await request_queue.coro_get()
-
-		# Schedule packet forwarding
-		asyncio.ensure_future(forward(data, addr, conns))
-
-async def forward(data, addr, conns):
-	# Select upstream server to forward to
-	index = random.randrange(len(upstreams))
-
-	# Await upstream forwarding coroutine
-	data = await upstream_forward(upstreams[index], data, conns[index])
-
-	# Send response to the listener
-	await response_queue.coro_put((data, addr))
+	loop.close()
 
 
 class UdpDohProtocol(asyncio.DatagramProtocol):
@@ -122,20 +82,20 @@ class UdpDohProtocol(asyncio.DatagramProtocol):
 		self.transport = transport
 
 	def datagram_received(self, data, addr):
-		# Schedule packet forwarding
-		asyncio.ensure_future(self.forward(data, addr))
+		# Schedule packet forwarding coroutine
+		asyncio.ensure_future(self.forward_packet(data, addr))
 
 	def error_received(self, exc):
 		logging.warning('Minor transport error')
 
-	async def forward(self, data, addr):
-		# Send request to forwarder
-		await request_queue.coro_put((data, addr))
+	async def forward_packet(self, data, addr):
+		# Select upstream server to forward to
+		index = random.randrange(len(upstreams))
 
-		# Receive response from forwarder
-		data, addr = await response_queue.coro_get()
+		# Await upstream forwarding coroutine
+		data = await upstream_forward(upstreams[index], data, conns[index])
 
-		# Send response to the client
+		# Send DNS packet to client
 		self.transport.sendto(data, addr)
 
 
@@ -148,8 +108,8 @@ class TcpDohProtocol(asyncio.Protocol):
 		self.transport = transport
 
 	def data_received(self, data):
-		# Schedule packet forwarding
-		asyncio.ensure_future(self.forward(data))
+		# Schedule packet forwarding coroutine
+		asyncio.ensure_future(self.forward_packet(data))
 
 	def eof_received(self):
 		if self.transport.can_write_eof():
@@ -158,18 +118,18 @@ class TcpDohProtocol(asyncio.Protocol):
 	def connection_lost(self, exc):
 		self.transport.close()
 
-	async def forward(self, data):
-		# Send request to forwarder (remove 16-bit length prefix)
-		await request_queue.coro_put((data[2:], None))
+	async def forward_packet(self, data):
+		# Select upstream server to forward to
+		index = random.randrange(len(upstreams))
 
-		# Receive response from forwarder
-		data, _ = await response_queue.coro_get()
+		# Await upstream forwarding coroutine (remove 16-bit length prefix)
+		data = await upstream_forward(upstreams[index], data[2:], conns[index])
 
-		# Send response to the client (add 16-bit length prefix)
+		# Send DNS packet to client (add 16-bit length prefix)
 		self.transport.write(struct.pack('! H', len(data)) + data)
 
 
-async def upstream_connect():
+async def upstream_connect(url):
 	"""
 	Create an upstream connection that will later be bound to a url.
 
@@ -178,7 +138,9 @@ async def upstream_connect():
 	"""
 
 	# Create connection with default DNS message headers
-	return aiohttp.ClientSession(headers=headers)
+	conn = vibora.client.Session(headers=headers)
+
+	return conn
 
 
 async def upstream_forward(url, data, conn):
@@ -199,27 +161,13 @@ async def upstream_forward(url, data, conn):
 		https://developers.cloudflare.com/1.1.1.1/dns-over-https/wireformat/
 	"""
 
-	disconnected = False
-
 	# Await upstream response
 	while True:
-		try:
-			# Attempt to query the upstream server asynchronously
-			async with conn.post(url, data=data) as response:
-				if disconnected == True:
-					logging.info('Reconnected to upstream server: %s' % (url))
-					disconnected = False
+		response = await conn.post(url, body=data, decode=False)
+		if response.status_code == 200:
+			return response.content
 
-				if response.status == 200:
-					return await response.read()
-
-				# Log abnormal HTTP status codes
-				logging.warning('%s (%d): IN %s, OUT %s' % (url, response.status, data, await response.read()))
-
-		# Log connection errors (aiohttp should attempt to reconnect on next request)
-		except aiohttp.ClientConnectionError:
-			logging.error('Connection error with upstream server: %s' % (url))
-			disconnected = True
+		logging.warning('%s (%d) IN %s, OUT %s' % (url, response.status_code, data, response.content))
 
 
 async def upstream_close(conn):
@@ -230,7 +178,7 @@ async def upstream_close(conn):
 		conn - aiohttp session object to close
 	"""
 
-	await conn.close()
+	conn.close()
 
 
 if __name__ == '__main__':
