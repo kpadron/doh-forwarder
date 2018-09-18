78a893225f13da7aad0ebb91fe674ecc94526c59
Kristian Padron <kpadron.github@gmail.com>
2018-09-17 20:38:07 -0400

    updated forwarder to have active cache

--- doh-forwarder.py
+++ doh-forwarder.py
@@ -4,6 +4,7 @@ import random, struct
 import argparse, logging
 import dns.message
 import dns.resolver
+import time
 
 # Attempt to use uvloop if installed for extra performance
 try:
@@ -22,14 +23,16 @@ parser.add_argument('-u', '--upstreams', nargs='+', default=['https://1.1.1.1/dn
 					help='upstream servers to forward DNS queries and requests to (default: %(default)s)')
 parser.add_argument('-t', '--tcp', action='store_true', default=False,
 					help='serve TCP based queries and requests along with UDP (default: %(default)s)')
+parser.add_argument('--no-cache', action='store_true', default=False,
+					help='don\'t cache answers from upstream servers (default: %(default)s)')
+parser.add_argument('--active-cache', action='store_true', default=False,
+					help='actively replace expired entries by making autonomous requests to the upstream servers (default: %(default)s)')
 args = parser.parse_args()
 
 host = args.listen_address
 port = args.listen_port
 upstreams = args.upstreams
-
 headers = {'accept': 'application/dns-message', 'content-type': 'application/dns-message'}
-conns = []
 
 
 def main():
@@ -41,23 +44,25 @@ def main():
 
 	# Connect to upstream servers
 	resolvers = []
-	for upstream in upstreams:
-		logging.info('Connecting to upstream server: %s' % (upstream))
-		cache = dns.resolver.LRUCache(10000)
-		resolver = DohResolver(upstream)
-		resolver.connect(headers, 4)
-		resolvers.append(resolver)
+	cache = dns.resolver.LRUCache(50000)
+	logging.info('Connecting to upstream servers: %r' % (upstreams))
+	resolver = DohResolver(upstreams, cache)
+	loop.run_until_complete(resolver.connect(headers))
 
 	# Setup UDP server
 	logging.info('Starting UDP server listening on: %s#%d' % (host, port))
-	udp_listen = loop.create_datagram_endpoint(lambda: UdpDohProtocol(resolvers[0]), local_addr = (host, port))
+	udp_listen = loop.create_datagram_endpoint(lambda: UdpDohProtocol(resolver), local_addr = (host, port))
 	udp, protocol = loop.run_until_complete(udp_listen)
 
 	# Setup TCP server
 	if args.tcp:
 		logging.info('Starting TCP server listening on %s#%d' % (host, port))
-		tcp_listen = loop.create_server(TcpDohProtocol, host, port)
+		tcp_listen = loop.create_server(lambda: TcpDohProtocol(resolver), host, port)
 		tcp = loop.run_until_complete(tcp_listen)
+
+	# Setup cache worker
+	if args.active_cache:
+		asyncio.ensure_future(resolver.worker(10))
 
 	# Serve forever
 	try:
@@ -66,8 +71,7 @@ def main():
 		pass
 
 	# Close upstream connections
-	for conn in conns:
-		loop.run_until_complete(upstream_close(conn))
+	loop.run_until_complete(resolver.close())
 
 	# Close listening servers and event loop
 	udp.close()
@@ -76,88 +80,6 @@ def main():
 
 	loop.close()
 
-
-class DohResolver:
-	"""
-	DNS over HTTPS resolver class.
-	"""
-
-	def __init__(self, upstream, cache=None):
-		self.upstream = upstream
-		self.conn = None
-		self.cache = cache
-
-	def connect(self, headers, timeout):
-		"""
-		Prepare connection object corresponding to upstream server.
-		"""
-
-		# Create connection with default DNS message headers
-		# timeout = aiohttp.ClientTimeout(total=timeout)
-		# connector = aiohttp.TCPConnector(keepalive_timeout=60, limit=0, limit_per_host=200, enable_cleanup_closed=True)
-		# self.conn = aiohttp.ClientSession(connector=connector, timeout=timeout, headers=headers)
-		self.conn = aiohttp.ClientSession(headers=headers)
-
-	async def close(self):
-		"""
-		Close connection to upstream server.
-		"""
-
-		await self.conn.close()
-
-	async def resolve(self, query):
-		"""
-		Perform DNS over HTTPS lookup.
-
-		Params:
-			query - normal wireformat DNS query
-
-		Returns:
-			A normal wireformat DNS answer.
-		"""
-
-		# Check cache if necessary
-		if self.cache is not None:
-			# Convert wireformat to message object
-			request = dns.message.from_wire(query)
-			id = request.id
-			request = request.question[0]
-			request = (request.name, request.rdtype, request.rdclass)
-
-			# Return cached entry if possible
-			cached = self.cache.get(request)
-			if cached is not None:
-				cached.response.id = id
-				answer = cached.response.to_wire()
-				return answer
-
-		# Await upstream response
-		while True:
-			try:
-				# Attempt to query the upstream server asynchronously
-				async with self.conn.post(self.upstream, data=query) as http:
-					if http.status == 200:
-						answer = await http.read()
-
-						# Add answer to cache if necessary
-						if self.cache is not None:
-							response = dns.message.from_wire(answer)
-							response = dns.resolver.Answer(*request, response, False)
-							self.cache.put(request, response)
-
-						return answer
-
-					# Log abnormal HTTP status codes
-					logging.warning('%s (%d): IN %s, OUT %s' % (self.upstream, http.status, query, await http.read()))
-
-			# Log client connection errors (aiohttp should attempt to reconnect on next request)
-			except aiohttp.ClientConnectionError as exc:
-				logging.error('Client error, %s: %s' % (self.upstream, exc))
-
-			# Log request timeout errors
-			except asyncio.TimeoutError as exc:
-				logging.error('Timeout error, %s: %s' % (self.upstream, exc))
-				return b''
 
 class UdpDohProtocol(asyncio.DatagramProtocol):
 	"""
@@ -172,18 +94,13 @@ class UdpDohProtocol(asyncio.DatagramProtocol):
 		self.transport = transport
 
 	def datagram_received(self, data, addr):
-		# Schedule packet forwarding coroutine
 		asyncio.ensure_future(self.process_packet(data, addr))
 
 	def error_received(self, exc):
 		logging.warning('Minor transport error')
 
 	async def process_packet(self, query, addr):
-		# Select upstream server to forward to
-		# index = random.randrange(len(upstreams))
-
 		# Await upstream forwarding coroutine
-		# data = await upstream_forward(upstreams[index], data, conns[index])
 		answer = await self.resolver.resolve(query)
 
 		# Send DNS packet to client
@@ -195,12 +112,15 @@ class TcpDohProtocol(asyncio.Protocol):
 	DNS over HTTPS TCP protocol to use with asyncio.
 	"""
 
+	def __init__(self, resolver):
+		self.resolver = resolver
+		super().__init__()
+
 	def connection_made(self, transport):
 		self.transport = transport
 
 	def data_received(self, data):
-		# Schedule packet forwarding coroutine
-		asyncio.ensure_future(self.forward_packet(data))
+		asyncio.ensure_future(self.process_packet(data))
 
 	def eof_received(self):
 		if self.transport.can_write_eof():
@@ -209,80 +129,153 @@ class TcpDohProtocol(asyncio.Protocol):
 	def connection_lost(self, exc):
 		self.transport.close()
 
-	async def forward_packet(self, data):
-		# Select upstream server to forward to
-		index = random.randrange(len(upstreams))
-
+	async def process_packet(self, query):
 		# Await upstream forwarding coroutine (remove 16-bit length prefix)
-		data = await upstream_forward(upstreams[index], data[2:], conns[index])
+		answer = await self.resolver.resolve(query[2:])
 
 		# Send DNS packet to client (add 16-bit length prefix)
 		self.transport.write(struct.pack('! H', len(data)) + data)
 
 
-async def upstream_connect():
+class DohResolver:
 	"""
-	Create an upstream connection that will later be bound to a url.
-
-	Returns:
-		A aiohttp session object
+	DNS over HTTPS resolver class.
 	"""
 
-	# Create connection with default DNS message headers
-	timeout = aiohttp.ClientTimeout(total=4)
-	connector = aiohttp.TCPConnector(keepalive_timeout=60, limit=0, limit_per_host=200, enable_cleanup_closed=True)
-	conn = aiohttp.ClientSession(connector=connector, timeout=timeout, headers=headers)
-	return conn
+	def __init__(self, upstreams, cache=None):
+		self.upstreams = upstreams
+		self.conn = None
+		self.cache = cache
 
+	async def connect(self, headers):
+		"""
+		Prepare connection object corresponding to upstream servers.
+		"""
 
-async def upstream_forward(url, data, conn):
-	"""
-	Send a DNS request over HTTPS using POST method.
+		# Create connection with default DNS message headers
+		connector = aiohttp.TCPConnector(keepalive_timeout=60, limit=0, limit_per_host=200, enable_cleanup_closed=True)
+		self.conn = aiohttp.ClientSession(connector=connector, headers=headers)
 
-	Params:
-		url  - url to forward queries to
-		data - normal DNS packet data to forward
-		conn - HTTPS connection to upstream DNS server
+	async def close(self):
+		"""
+		Close connection to upstream server.
+		"""
 
-	Returns:
-		A normal DNS response packet from upstream server
+		await self.conn.close()
 
-	Notes:
-		Using DNS over HTTPS POST format as described here:
-		https://tools.ietf.org/html/draft-ietf-doh-dns-over-https-12
-		https://developers.cloudflare.com/1.1.1.1/dns-over-https/wireformat/
-	"""
+	async def resolve(self, query, update=False):
+		"""
+		Resolve a DNS query using a cache or upstream server.
 
-	# Await upstream response
-	while True:
-		try:
+		Params:
+			query  - normal wireformat DNS query
+			update - only update cache entry for this query
+
+		Returns:
+			A normal wireformat DNS answer.
+		"""
+
+		# Check cache if necessary
+		if self.cache and not update:
+			# Convert wireformat to message object
+			request = dns.message.from_wire(query)
+			id = request.id
+			request = request.question[0]
+			request = (request.name, request.rdtype, request.rdclass)
+
+			# Return cached entry if possible
+			cached = self.cache.get(request)
+			if cached:
+				cached.response.id = id
+				answer = cached.response.to_wire()
+				return answer
+
+		# Resolve via upstream server
+		answer = await self.forward(query)
+
+		# Add answer to cache if necessary
+		if self.cache:
+			if update:
+				# Convert wireformat to message object
+				request = dns.message.from_wire(query)
+				id = request.id
+				request = request.question[0]
+				request = (request.name, request.rdtype, request.rdclass)
+
+			response = dns.message.from_wire(answer)
+			response = dns.resolver.Answer(*request, response, False)
+			self.cache.put(request, response)
+
+		return answer
+
+	async def forward(self, query, timeout=0):
+		"""
+		Perform DNS over HTTPS lookup using POST method.
+
+		Params:
+			query - normal wireformat DNS query
+
+		Returns:
+			A normal wireformat DNS answer.
+
+		Notes:
+			Using DNS over HTTPS POST format as described here:
+			https://tools.ietf.org/html/draft-ietf-doh-dns-over-https-12
+			https://developers.cloudflare.com/1.1.1.1/dns-over-https/wireformat/
+		"""
+
+		# Await upstream response
+		index = 0
+		while True:
 			# Attempt to query the upstream server asynchronously
-			async with conn.post(url, data=data) as response:
-				if response.status == 200:
-					return await response.read()
+			try:
+				async with self.conn.post(self.upstreams[index], data=query) as http:
+					if http.status == 200:
+						return await http.read()
 
-				# Log abnormal HTTP status codes
-				logging.warning('%s (%d): IN %s, OUT %s' % (url, response.status, data, await response.read()))
+					# Log abnormal HTTP status codes
+					logging.warning('%s (%d): IN %s, OUT %s' % (self.upstreams[index], http.status, query, await http.read()))
 
-		# Log client connection errors (aiohttp should attempt to reconnect on next request)
-		except aiohttp.ClientConnectionError as exc:
-			logging.error('Client error, %s: %s' % (url, exc))
+			# Log client connection errors (aiohttp should attempt to reconnect on next request)
+			except aiohttp.ClientConnectionError as exc:
+				logging.error('Client error, %s: %s' % (self.upstreams[index], exc))
 
-		# Log request timeout errors
-		except asyncio.TimeoutError as exc:
-			logging.error('Timeout error, %s: %s' % (url, exc))
-			return b''
+			# Log request timeout errors
+			except asyncio.TimeoutError as exc:
+				logging.error('Timeout error, %s: %s' % (self.upstreams[index], exc))
+				return b''
+
+			index = (index + 1) % len(self.upstreams)
 
 
-async def upstream_close(conn):
-	"""
-	Close an upstream connection.
+	async def worker(self, period, max=1000):
+		"""
+		Worker to process cache entries and preemptively replace expiring entries.
 
-	Params:
-		conn - aiohttp session object to close
-	"""
+		Params:
+			period - time to wait between cache scans (in seconds)
+			max    - maximum number of concurrent autonomous requests (0 means unlimited)
 
-	await conn.close()
+		"""
+
+		while True:
+			expiring = 0
+
+			for request, response in self.cache.data.items():
+				if response.value.expiration > (time.time() + period):
+					continue
+
+				query = dns.message.make_query(*request).to_wire()
+				asyncio.ensure_future(self.resolve(query, True))
+				expiring += 1
+
+				if max and expiring >= max:
+					break
+
+			if expiring > 0:
+				logging.info('Cache, updating %d/%d' % (expiring, len(self.cache.data)))
+
+			await asyncio.sleep(period)
 
 
 if __name__ == '__main__':
